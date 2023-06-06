075a8bcdf6481a59078b9f45fa24e2a8d1a61022
KAL <kevin12147@yahoo.com.tw>
2023-06-07 02:23:59 +0800

    image hover effect added

--- app/(site)/components/Gallery.tsx
+++ app/(site)/components/Gallery.tsx
@@ -11,6 +11,9 @@ interface photo {
 
 export default function Gallery() {
   const [photos, setPhotos] = useState<photo[]>([]);
+  const [state, setState] = useState<any>({
+    backgroundPosition: `0% 0%`,
+  });
   const options = {
     headers: { "content-type": "utf-8" },
   };
@@ -21,17 +24,26 @@ export default function Gallery() {
     });
   }, [photos]);
 
+  const handleMouseMove = (e: any) => {
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    const x = ((e.pageX - left) / width) * 100;
+    const y = ((e.pageY - top) / height) * 100;
+    setState({ backgroundPosition: `${x}% ${y}%` });
+  };
+
   return (
     <div className="flex w-full h-full flex-wrap my-5 gap-1">
       {photos.map((photo, index) => (
-        <div
-          key={photo.id}
-          className="relative w-[32%] border-solid border-white border-2 shadow-2xl shadow-black"
-        >
-          <div className="relative h-0 pb-[55%] overflow-hidden">
+        <div className="basis-[100%] md:basis-[calc(50%-2px)] lg:basis-[calc(33%-1.5px)] border-solid border-white border-2 shadow-2xl shadow-black">
+          <div
+            key={photo.id}
+            className="group relative h-0 pb-[55%] bg-no-repeat overflow-hidden"
+            style={{ ...state, backgroundImage: `url(${photo.image}` }}
+            onMouseMove={(e) => handleMouseMove(e)}
+          >
             <img
               src={photo.image}
-              className="absolute inset-0 w-full h-full hover:scale-150"
+              className="absolute top-0 w-full h-full group-hover:opacity-0"
               alt={photo.image}
             />
           </div>
