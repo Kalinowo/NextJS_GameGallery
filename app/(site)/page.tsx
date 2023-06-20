b9301102fffedafba193159a9c0d89c17712a88d
KAL <kevin12147@yahoo.com.tw>
2023-06-20 12:01:59 +0800

    attmp 2

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
 
   useEffect(() => {
     axios.get("/api/image/get", options).then((res) => {
+      console.log(res.data);
       console.log("sending get request");
       setPhotos(res.data.reverse());
     });
